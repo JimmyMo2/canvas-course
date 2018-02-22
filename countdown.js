7f2bf5a65a50b64093accdcb9f1f244ff4b21e4e
JimmyMo2 <32846008+JimmyMo2@users.noreply.github.com>
2018-02-22 15:36:50 +0800

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -4,7 +4,7 @@ var RADIUS = 8;
 var MARGIN_TOP = 60;
 var MARGIN_LEFT = 30;
 
-const endTime = new Date(2018,3,20,22,47,52);
+const endTime = new Date(2018,4,20,22,47,52);
 var curShowTimeSeconds = 0;
 
 var balls = [];
